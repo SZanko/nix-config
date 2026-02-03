8daf99228f833c97aa4f859c0ef12abc898c37f2
SZanko <szanko_dev@proton.me>
2026-02-03 13:41:29 +0000

    Change default port for ipfs Gateway
    Signed-off-by: SZanko <szanko_dev@proton.me>

--- modules/services/ipfs.nix
+++ modules/services/ipfs.nix
@@ -3,6 +3,11 @@
     services.kubo = {
       enable = true;
       package = pkgs.unstable.kubo;
+      settings = {
+        Addresses = {
+          Gateway = "/ip4/127.0.0.1/tcp/9053";
+        };
+      };
     };
   };
 }
