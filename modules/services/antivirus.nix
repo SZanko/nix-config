5252e9c1d18cfbbad95c60e6576b74d70da269ec
SZanko <szanko_dev@proton.me>
2026-06-27 19:12:21 +0100

    Add opensnitch rule for clamav
    Signed-off-by: SZanko <szanko_dev@proton.me>

--- modules/services/antivirus.nix
+++ modules/services/antivirus.nix
@@ -1,5 +1,8 @@
+{ inputs
+, ...
+}:
 {
-  flake.modules.nixos.antivirus = {
+  flake.modules.nixos.antivirus = { pkgs, lib, ...}: {
     services = {
       clamav = {
         scanner.enable = true;
@@ -11,6 +14,22 @@
         };
         fangfrisch = {
           enable = true;
+        };
+      };
+
+      opensnitch.rules = {
+        clamav = {
+          created= "2026-06-27T19=00=20.000000Z";
+          name= "clamav";
+          enabled= true;
+          action= "allow";
+          duration= "always";
+          operator = {
+            type= "simple";
+            sensitive = false;
+            operand= "process.path";
+            data= "${lib.getBin pkgs.clamav}/bin/freshclam";
+          };
         };
       };
     };
